6e8ccf41a7c465497518ad0a83404a6d5fe0c828
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-12-29 13:48:15 +0000

    Deploying to gh-pages from @ GoitStudentsWorks/js-95-SvitlanaHu@0b5cf68a6063738e6850cef0ac1779298bc2dd28 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,32 @@
+import{g as d,s as b,i as y,c as g,u as f}from"./assets/scrollup-9bd49462.js";document.getElementById("close-my-modal-btn").addEventListener("click",k);function k(){document.getElementById("my-modal").classList.remove("open")}window.addEventListener("keydown",t=>{t.key==="Escape"&&document.getElementById("my-modal").classList.remove("open")});document.querySelector("#my-modal .modal__box").addEventListener("click",t=>{t._isClickWithInModal=!0});document.getElementById("my-modal").addEventListener("click",t=>{t._isClickWithInModal||t.currentTarget.classList.remove("open")});document.querySelector(".order-form");const p=document.querySelector(".img-cart-modal");document.querySelector(".order-btn-submit");document.querySelector(".close-svg");document.querySelector(".checkout-modal");const e={cartProductList:document.querySelector(".js-cart-list"),deleteAllBtn:document.querySelector(".js-delete-all-btn"),emptyBasketContent:document.querySelector(".empty-basket-content"),emptyBasketWrap:document.querySelector(".empty-basket-wrap"),cartMainContainer:document.querySelector(".main-content-with-delete-all"),numberOfProducts:document.querySelector(".js-number-of-products"),cartTotalPrice:document.querySelector(".js-cart-amount"),orderForm:document.querySelector(".order-form")};v();l();I();e.cartProductList.addEventListener("click",C);function v(){const t=d();if(!t.length){e.emptyBasketContent.hidden=!1;return}S(t),e.cartMainContainer.hidden=!1,e.emptyBasketWrap.style.display="none",u(),e.orderForm.addEventListener("submit",B)}function l(){const t=d();e.numberOfProducts.textContent=t.length}function C(t){if(!t.target.classList.contains("js-delete-product-btn"))return;const n=t.target.closest(".js-cart-item"),r=n.dataset.id,o=d().filter(({_id:s})=>r!==s);b(o),n.style.display="none",l(),L(),u()}function L(){d().length||(e.cartMainContainer.hidden=!0,e.emptyBasketContent.hidden=!1,e.emptyBasketWrap.style.display="block",e.cartProductList.innerHTML="")}function S(t){const n=t.map(({_id:r,name:a,img:o,category:s,price:m,size:c})=>{const i=s.split("_").join(" ");return`<li class="cart-item js-cart-item" data-id = ${r}>
+       <span class="delete-product-btn js-delete-product-btn">
+              <svg width="12" height="12" class='js-delete-product-btn'>
+                <use class='js-delete-product-btn' href="${y}#close-button"></use></svg
+            ></span>
+          <div class="cart-item-info-wrap">
+            <div class="cart-img-wrap-bgc">
+              <div class="cart-img-thumb">
+              <img
+                src="${o}"
+                alt="${a}"
+                width="100"
+                height="100"
+              />
+              </div>
+            </div>
+            <div class="cart-item-descr">
+              <h3 class="cart-item-title">${a}</h3>
+              <div class="cart-text-wrap">
+                <p class="cart-item-text">
+                  <span class="cart-light-text">Category:</span
+                  >&nbsp;&nbsp;${i}
+                </p>
+                <p class="cart-item-text">
+                  <span class="cart-light-text">Size:</span>&nbsp;&nbsp;${c}
+                </p>
+              </div>
+              <p class="cart-item-price">$${m}</p>
+            </div>
+          </div>
+        </li>`}).join("");return e.cartProductList.innerHTML=n}e.deleteAllBtn.addEventListener("click",h);function h(){e.cartProductList.innerHTML="",g(),u(),l(),e.cartMainContainer.hidden=!0,e.emptyBasketContent.hidden=!1,e.emptyBasketWrap.style.display="block"}function u(){const n=d().reduce((r,{price:a,quantity:o})=>r+=a*o,0);e.cartTotalPrice.textContent=n.toFixed(2),f()}async function B(t){t.preventDefault();const n=t.currentTarget.elements.email.value;if(!n){alert("Please, write your email!");return}t.currentTarget.reset();const a=d().map(({_id:s})=>({productId:s,amount:1})),o={email:n,products:a};try{let s=function(){document.getElementById("my-modal").classList.add("open")};document.getElementById("open-modal-btn").addEventListener("click",s);const c=await fetch("https://food-boutique.b.goit.study/api/orders",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify(o)});if(c.ok)s(),g(),h(),f();else if(c.status===400){const i=await c.json();alert("Bad request: "+i.message)}else if(c.status===404)alert("Resource not found.");else if(c.status===500)alert("Server error. Please try again later.");else throw new Error("Failed to subscribe.")}catch(s){alert("Error: "+s.message)}}function I(){const t=d();P(t)}function P(t){const n=t.map(({img:r,name:a})=>({img:r,name:a}));if(n.length===1){const r=n[0],a=`<img src="${r.img}" alt="${r.name}" width="140" height="140">`;p.insertAdjacentHTML("beforeend",a)}else{const r=`<svg class="success-svg" width="140" height="140"><use href="${y}#icon-order-placed-purchased-icon"></use></svg>`;p.insertAdjacentHTML("beforeend",r)}}
+//# sourceMappingURL=commonHelpers.js.map
---
+++ commonHelpers2.js
@@ -0,0 +1,154 @@
+import{a as u,i,b as p,d as b,e as m}from"./assets/scrollup-9bd49462.js";import{a as h,S as d,P as z}from"./assets/vendor-f5f845be.js";const $="https://food-boutique.b.goit.study/api/products";async function q(){try{return(await h.get(`${$}/categories`)).data}catch(t){throw console.error("Error fetching categories:",t),t}}function k({page:t,limit:e,keyword:o,category:n,byABC:s,byPrice:c,byPopularity:a}){const r={page:t,limit:e,...o&&{keyword:o},...n&&{category:n},...s&&{byABC:s},...c&&{byPrice:c},...a&&{byPopularity:a}};return h.get(`${$}`,{params:r})}async function w(t){try{return(await h.get(`${$}/${t}`)).data}catch(e){throw console.error("Error fetching product by id:",e),e}}const x=document.querySelector(".loader"),_=document.getElementById("main-content");function F(){const t=document.getElementById("filters-section");t&&t.scrollIntoView({behavior:"smooth"})}function B(){x.removeAttribute("hidden"),Array.from(_.children).forEach(t=>{t.querySelector(".search-container")||(t.style.filter="blur(5px) grayscale(1)")})}function A(){x.setAttribute("hidden",""),Array.from(_.children).forEach(t=>{t.style.filter="none"}),F()}document.addEventListener("DOMContentLoaded",function(){const t=document.getElementById("discount-products");document.getElementById("pagination");async function e(s){try{const a=(await h.get("https://food-boutique.b.goit.study/api/products/discount?page=${page}&limit=${itemsPerPage}`")).data;t.innerHTML="",a.forEach(r=>{const l=o(r);t.innerHTML+=l}),a.forEach(r=>{document.querySelector(`[data-product-id="${r._id}"]`).addEventListener("click",()=>T(r._id,a))}),u(a,".cart-btn-list-discount",i),p(a,".cart-btn-list-discount",i)}catch(c){console.error("Error fetching discount products:",c)}}e();function o(s){const c=s.name,a=12,r=window.innerWidth,l=c.length>a&&r>=1440,E=n(c,a);return`
+        <li class="card-container-list-discount" id="${s._id}" data-product-id="${s._id}">
+        <div class="photo-card-list-discount">
+                <div class="product-modal-list-discount">
+                    <div class="img-container-list-discount">
+                        <svg width="60" height="60" class="product-image-discount">
+                            <use href="${i}#icon-discount-green"></use>
+                         </svg>
+                        <img class="product-image-list-discount" src="${s.img}" alt="${s.name} photo" width=114 height=114 loading="lazy" />
+                    </div>
+                    </div>
+                    <div class="product-info-list">
+                        <div class="price-and-btn-list-discount">
+                        <h2 class="product-name-list-discount${l?" show-full-text":""}" title="${l?c:""}">${E}</h2>
+                        <h2 class="price-discount">$${s.price}</h2>
+                            <button class='cart-btn-list-discount' type="button" data-product-id="${s._id}">          
+                                <svg class='list-cart-svg-list' width="18" height="18" >
+                                    <use href="${i}#icon-heroicons-solid_shopping-cart-18x18"></use>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+            </div>
+        </li>
+        `}function n(s,c){return window.innerWidth>=1440&&s.length>c?`${s.slice(0,c)}...`:s}});const g=document.getElementById("discount-products");g.addEventListener("mouseover",function(){g.style.overflowY="auto"});g.addEventListener("mouseout",function(){g.style.overflowY="hidden"});async function T(t,e){try{const o=await w(t);D(o,e)}catch(o){console.error("Error fetching product details:",o)}}function D(t,e){d.fire({html:`
+        <div class="modal-product-container">
+          <div class="modal-image-container">
+            <img src="${t.img}" alt="${t.name}">
+          </div>
+          <div class="modal-product-info">
+            <h2 class="modal-product-title">${t.name}</h2>
+            <p><span class="modal-product-text">Category:</span> <span class="modal-product-value">${t.category}</span></p>
+            <p><span class="modal-product-text">Size:</span> <span class="modal-product-value">${t.size}</span></p>
+            <p><span class="modal-product-text">Popularity:</span> <span class="modal-product-value">${t.popularity}</span></p>
+            <p class="modal-product-description">${t.desc}</p>
+          </div>
+        </div>
+        <div class="modal-price-button-container">
+          <p class="modal-product-price">$${t.price}</p>
+          <button class='modal-add-to-cart-btn' type="button" data-product-id="${t._id}">
+            Add to 
+            <svg class="modal-add-to-cart-svg" width="18" height="18">
+              <use href="${i}#icon-heroicons-solid_shopping-cart-18x18"></use>
+            </svg>
+          </button>
+        </div>
+        <svg class="custom-close-icon" width="28" height="28">
+          <use href="${i}#icon-close-sharp"></use>
+        </svg>
+      `,showConfirmButton:!1,customClass:{container:"custom-swal"}}),p(e,".modal-add-to-cart-btn",i),u(e,".modal-add-to-cart-btn",i),document.querySelector(".custom-close-icon").addEventListener("click",()=>{d.close()})}const W="https://food-boutique.b.goit.study/api/products/popular";async function H(){try{return(await h.get(`${W}`)).data}catch(t){throw console.error("Error fetching popular products:",t),t}}function M(t){const e=document.querySelector(".popular-product-list");e&&t.forEach(o=>{const n=document.createElement("li");n.classList.add("popular-product-item");const s=o.category.split("_").join(" ");n.innerHTML=`
+      <div class="popular-modal"  data-product-id="${o._id}">
+      <div class="popular-img">
+        <img class="popular-photo-item" src="${o.img}" alt="${o.name}" width="56" height="56" loading="lazy">
+      </div>  
+      <ul class="about-popular">
+        <li class="name-popular-product">${o.name}</li>
+        <li class="subname-popular-product">Category: <span class="id-subname">${s}</span></li>
+        <li class="subname-popular-product">Size: <span class="id-subname">${o.size}</span></li>
+        <li class="subname-popular-product">Popularity: <span class="id-subname">${o.popularity}</span></li>
+      </ul>
+      <button class='popular-cart-btn' type="button" data-product-id="${o._id}">
+        <svg class="list-cart-svg-list" width="18" height="18">
+          <use href="${i}#icon-heroicons-solid_shopping-cart-18x18"></use>
+        </svg>
+      </button>
+    </div>
+    
+      `,e.appendChild(n)}),t.forEach(o=>{document.querySelector(`[data-product-id="${o._id}"]`).addEventListener("click",()=>U(o._id,t))}),u(t,".popular-cart-btn",i),p(t,".popular-cart-btn",i)}H().then(t=>{M(t)}).catch(t=>{console.error("Error:",t)});async function U(t,e){try{const o=await w(t);R(o,e)}catch(o){console.error("Error fetching product details:",o)}}function R(t,e){d.fire({html:`
+        <div class="modal-product-container">
+          <div class="modal-image-container">
+            <img src="${t.img}" alt="${t.name}">
+          </div>
+          <div class="modal-product-info">
+            <h2 class="modal-product-title">${t.name}</h2>
+            <p><span class="modal-product-text">Category:</span> <span class="modal-product-value">${t.category}</span></p>
+            <p><span class="modal-product-text">Size:</span> <span class="modal-product-value">${t.size}</span></p>
+            <p><span class="modal-product-text">Popularity:</span> <span class="modal-product-value">${t.popularity}</span></p>
+            <p class="modal-product-description">${t.desc}</p>
+          </div>
+        </div>
+        <div class="modal-price-button-container">
+          <p class="modal-product-price">$${t.price}</p>
+          <button class='modal-add-to-cart-btn' type="button" data-product-id="${t._id}">
+            Add to 
+            <svg class="modal-add-to-cart-svg" width="18" height="18">
+              <use href="${i}#icon-heroicons-solid_shopping-cart-18x18"></use>
+            </svg>
+          </button>
+        </div>
+        <svg class="custom-close-icon" width="28" height="28">
+          <use href="${i}#icon-close-sharp"></use>
+        </svg>
+      `,showConfirmButton:!1,customClass:{container:"custom-swal"}}),p(e,".modal-add-to-cart-btn",i),u(e,".modal-add-to-cart-btn",i),document.querySelector(".custom-close-icon").addEventListener("click",()=>{d.close()})}const y=document.getElementById("products-list-container"),P=document.querySelector(".no-results-container");let L;window.addEventListener("resize",I);function I(){let t;window.innerWidth>=1440?t=9:window.innerWidth>=768?t=8:t=6,b().limit!==t&&(m("page",1),m("limit",t),v())}function j(){const t=document.getElementById("tui-pagination-container");t&&t.remove();const e=document.createElement("div");e.id="tui-pagination-container",e.className="tui-pagination",y.after(e)}async function v(){I();const t=b();let e=t.page||1,o=t.limit||6;B();try{const n=await k(t),{perPage:s,totalPages:c,results:a}=n.data,r=s*c;if(a.length===0){P.classList.remove("visually-hidden"),y.innerHTML="",document.getElementById("tui-pagination-container").classList.add("visually-hidden");return}y.innerHTML=Y(a),P.classList.add("visually-hidden"),a.forEach(l=>{document.querySelector(`[data-product-id="${l._id}"]`).addEventListener("click",()=>N(l._id,a))}),O(r,e,o),p(a,".cart-btn-list",i),u(a,".cart-btn-list",i)}catch(n){console.error("Error fetching products",n)}finally{A()}}function O(t,e,o){j();const n=document.getElementById("tui-pagination-container");if(t>o){const s=window.innerWidth<768?2:4;L=new z(n,{totalItems:t,itemsPerPage:o,visiblePages:s,centerAlign:!0,page:e}),L.on("beforeMove",c=>{m("page",c.page),v()})}else n.classList.add("visually-hidden")}function Y(t){return`<ul class="card-container-list">${t.map(e=>{const o=e.category.split("_").join(" ");return`
+        <li class="photo-card-list" data-product-id="${e._id}">
+          <div class="img-container-list">
+            <img class="product-image-list" src="${e.img}" alt="${e.name} photo" width=140 height=140 loading="lazy" />
+          </div>
+          <div class="product-info-list">
+            <h2 class="product-name-list">${e.name}</h2>
+            <div class='product-atributes-list'>
+              <p class="atributes-info-list">
+                <b class="atributes-list">Category:</b> ${o}
+              </p>
+              <p class="atributes-info-list">
+                <b class="atributes-list">Size:</b> ${e.size}
+              </p>
+              <p class="atributes-info-list">
+                <b class="atributes-list">Popularity:</b> ${e.popularity}
+              </p>
+            </div>
+            <div class="price-and-btn-list">
+              <h2 class="product-price-list">$${e.price}</h2>
+              <button class='cart-btn-list' type="button" data-product-id="${e._id}">
+                <svg class="list-cart-svg-list" width="18" height="18">
+                  <use href="${i}#icon-heroicons-solid_shopping-cart-18x18"></use>
+                </svg>
+              </button>
+            </div>
+          </div>
+        </li>`}).join("")}</ul>`}async function N(t,e){try{B();const o=await w(t);V(o,e)}catch(o){console.error("Error fetching product details:",o)}finally{A()}}function V(t,e){d.fire({html:`
+      <div class="modal-product-container">
+        <div class="modal-image-container">
+          <img class="modal-img" src="${t.img}" alt="${t.name}">
+        </div>
+        <div class="modal-product-info">
+          <h2 class="modal-product-title">${t.name}</h2>
+          <div class="modal-product-main-info>
+            <p class="text-box">
+              <span class="modal-product-text">Category:</span> <span class="modal-product-value">${t.category}</span>
+            </p>
+            <p class="text-box">
+              <span class="modal-product-text">Size:</span> <span class="modal-product-value">${t.size}</span>
+            </p>
+            <p class="text-box">
+              <span class="modal-product-text">Popularity:</span> <span class="modal-product-value">${t.popularity}</span>
+            </p>
+          </div>
+          <p id="modal-product-description" class="modal-product-description">${t.desc}</p>  
+        </div>
+      </div>
+      <div class="modal-price-button-container">
+        <p class="modal-product-price">$${t.price}</p>
+        <button class='modal-add-to-cart-btn' type="button" data-product-id="${t._id}">
+         Add to <span class="modal-button-text">Add to</span> 
+          <svg class="modal-add-to-cart-svg" width="18" height="18">
+            <use href="${i}#icon-heroicons-solid_shopping-cart-18x18"></use>
+          </svg>
+        </button>
+      </div>
+      <svg class="custom-close-icon" width="28" height="28">
+        <use href="${i}#icon-close-sharp"></use>
+      </svg>
+    `,showConfirmButton:!1,customClass:{container:"custom-swal"}}),p(e,".modal-add-to-cart-btn",i),u(e,".modal-add-to-cart-btn",i),document.querySelector(".custom-close-icon").addEventListener("click",()=>{d.close()})}async function C(t){for(const e in t)m(e,t[e]);m("page",1),await v()}async function G(){try{const t=await q(),e=document.getElementById("category-select");t.forEach(o=>{const n=document.createElement("option");n.value=o,n.textContent=o.replace(/_/g," "),e.appendChild(n)})}catch(t){console.error("Error fetching categories:",t)}}async function J(){const t=b();if(t.keyword&&(document.getElementById("search-bar-id").value=t.keyword),await G(),t.category&&(document.getElementById("category-select").value=t.category),t.byABC||t.byPrice||t.byPopularity){const e=document.getElementById("sorting-select");e.value=t.byABC?"byABC":t.byPrice?"byPrice":"byPopularity"}await v()}function K(){const t=document.getElementById("search-form"),e=t.elements,o=e["item-search-value"],n=e["category-select"],s=e["sorting-select"];t.addEventListener("submit",async a=>{a.preventDefault();const r=o.value.trim();await C({keyword:r||null})}),n.addEventListener("change",c),s.addEventListener("change",c),o.addEventListener("input",c);async function c(){const a={keyword:o.value.trim()||null,category:n.value!=="Show all"?n.value:null,byABC:s.value==="byABC",byPrice:s.value==="byPrice",byPopularity:s.value==="byPopularity"};await C(a)}}document.addEventListener("DOMContentLoaded",()=>{J(),K()});window.onscroll=()=>f();window.addEventListener("scroll",S(f,250)),window.addEventListener("resize",S(f,250));async function f(){const t=document.querySelector("body"),e=document.body.offsetHeight,o=window.innerHeight,n=window.scrollY,s=e-o/4;n+o>s?t.classList.add("body--no-transparency"):t.classList.remove("body--no-transparency")}function S(t,e){let o=null;return function(...s){o||(o=setTimeout(()=>{t(...s),clearTimeout(o),o=null},e))}}
+//# sourceMappingURL=commonHelpers2.js.map
